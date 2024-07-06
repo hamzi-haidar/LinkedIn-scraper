e697a62ec7c7b646fcee3150ea0c101b0d6d187c
Hamzi Haidar <hamzihaider@gmail.com>
2024-07-06 09:25:28 +0300

    scraped data and saved them in json file with json server

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,45 @@
 import puppeteer from "puppeteer";
 
-(async () => {
+const scraper = async (keyword) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await page.goto(
-    "https://www.linkedin.com/jobs/search?trk=guest_homepage-basic_guest_nav_menu_jobs&position=1&pageNum=0"
+    `https://www.linkedin.com/jobs/search?keywords=${keyword}&location=&geoId=&trk=public_jobs_jobs-search-bar_search-submit&original_referer=https%3A%2F%2Fwww.linkedin.com%2Fjobs%2Fsearch%3Fkeywords%3Dmlops%26location%3DUnited&position=1&pageNum=0`
   );
 
   await page.setViewport({ width: 1080, height: 1024 });
 
-  await page.locator("#job-search-bar-keywords").fill("mlops");
+  const jobsData = await page.evaluate(() => {
+    const jobs = document.querySelectorAll(".base-search-card__info");
+
+    return Array.from(jobs).map((job) => {
+      const title = job.querySelector("h3").innerText;
+      const companyName = job.querySelector("h4").innerText;
+      const jobLocation = job.querySelector("span").innerText;
+
+      return { title, companyName, jobLocation };
+    });
+  });
+
+  console.log(jobsData);
+
+  // npm run server in another terminal for this to work
+  jobsData.forEach(async (job) => {
+    await fetch("http://localhost:8000/linkedInJobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(job),
+    });
+  });
 
   await page.screenshot({
-    path: "hn.png",
+    path: "linkedIn.png",
   });
 
   await browser.close();
-})();
+};
+
+scraper("MLops");
